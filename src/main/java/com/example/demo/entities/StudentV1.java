77e0c0582eec0938556c8215f2876bc0374d6a5a
NguyenLinhPhuong <pnguyen9968@gmail.com>
2021-10-10 14:43:11 +0700

    task#04 demo version in REST API

---
+++ src/main/java/com/example/demo/entities/StudentV1.java
@@ -0,0 +1,25 @@
+package com.example.demo.entities;
+
+public class StudentV1 {
+	
+	private String name;
+
+	public StudentV1() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public StudentV1(String name) {
+		super();
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
